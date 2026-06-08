9c4acb20b705947ab53b01b35eb3f626ce6099ef
Torin O'Connor <tpoc19@gmail.com>
2026-06-08 14:58:51 -0500

    List method fix on PostRepository

--- backend/src/main/java/cuff/cuff_springboot/repository/PostRepository.java
+++ backend/src/main/java/cuff/cuff_springboot/repository/PostRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByStatus(String status);
    // Optional<Post> findById(int id);
-    List<Post> orderByCreatedAtDesc();
+    List<Post> findByOrderByCreatedAtDesc();
     List<Post> findByStatusOrderByCreatedAtDesc(String status);
     List<Post> findByCreatedBy_Id(Integer userId);
 }
