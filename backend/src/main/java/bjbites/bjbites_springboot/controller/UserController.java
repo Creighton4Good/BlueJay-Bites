95c265ec7345ee665bea53c9c3fc9d2ae07e6bed
Torin O'Connor <tpoc19@gmail.com>
2026-06-17 11:00:42 -0500

    Add and change endpoints/logic relating to updating user data

--- backend/src/main/java/bjbites/bjbites_springboot/controller/UserController.java
+++ backend/src/main/java/bjbites/bjbites_springboot/controller/UserController.java
@@ -1,6 +1,8 @@
 package bjbites.bjbites_springboot.controller;
 
+import bjbites.bjbites_springboot.entity.Role;
 import bjbites.bjbites_springboot.entity.User;
+import bjbites.bjbites_springboot.repository.RoleRepository;
 import bjbites.bjbites_springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +21,8 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     // Get all users (admin only)
     @PreAuthorize("hasAuthority('admin')")
@@ -52,6 +57,101 @@ public class UserController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    // Assign the admin
+    @PreAuthorize("hasAuthority('admin')")
+    @PutMapping("/{id}/admin/promote")
+    public ResponseEntity<User> promoteToAdmin(@PathVariable Integer id) {
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()) {
+            User user = userData.get();
+
+            if ("admin".equals(user.getRole().getRoleName())) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            Role adminRole = roleRepository.findByRoleName("admin")
+                    .orElseThrow(() -> new RuntimeException("Admin role not found"));
+            user.setRole(adminRole);
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Demote the admin
+    @PreAuthorize("hasAuthority('admin')")
+    @PutMapping("/{id}/admin/demote")
+    public ResponseEntity<User> demoteAdmin(@PathVariable Integer id) {
+        long adminCount = userRepository.countByRoleRoleName("admin");
+        if (adminCount <= 1) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()) {
+            User user = userData.get();
+
+            if (!"admin".equals(user.getRole().getRoleName())) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            Role userRole = roleRepository.findByRoleName("user")
+                    .orElseThrow(() -> new RuntimeException("user role not found"));
+            user.setRole(userRole);
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Assign the organizer
+    @PreAuthorize("hasAuthority('admin')")
+    @PutMapping("/{id}/event-organizer/promote")
+    public ResponseEntity<User> promoteToOrganizer(@PathVariable Integer id) {
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()) {
+            User user = userData.get();
+
+            if ("event_organizer".equals(user.getRole().getRoleName())) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            Role organizerRole = roleRepository.findByRoleName("event_organizer")
+                            .orElseThrow(() -> new RuntimeException("event organizer role not found"));
+            user.setRole(organizerRole);
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Demote the organizer
+    @PreAuthorize("hasAuthority('admin')")
+    @PutMapping("/{id}/event-organizer/demote")
+    public ResponseEntity<User> demoteOrganizer(@PathVariable Integer id) {
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()) {
+            User user = userData.get();
+
+            if (!"event_organizer".equals(user.getRole().getRoleName())) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            Role userRole = roleRepository.findByRoleName("user")
+                            .orElseThrow(() -> new RuntimeException("user role not found"));
+            user.setRole(userRole);
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     // Create new user (admin only)
     @PreAuthorize("hasAuthority('admin')")
     @PostMapping("/create")
@@ -65,18 +165,21 @@ public class UserController {
     }
 
     // Update user
-    @PreAuthorize("hasAuthority('admin')")
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User userDetails) {
         Optional<User> userData = userRepository.findById(id);
         if (userData.isPresent()) {
             User user = userData.get();
-            user.setEmail(userDetails.getEmail());
-            user.setDisplayName(userDetails.getDisplayName());
-            user.setRole(userDetails.getRole());
-            user.setEntraId(userDetails.getEntraId());
-            user.setAuthProvider(userDetails.getAuthProvider());
-            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+
+            if (Objects.equals(user.getId(), id)) {
+                // TODO: change userId to authenticated user
+
+                user.setEmail(userDetails.getEmail());
+                user.setDisplayName(userDetails.getDisplayName());
+                user.setEntraId(userDetails.getEntraId());
+                user.setAuthProvider(userDetails.getAuthProvider());
+                return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+            }
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
